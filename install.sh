ffa175421f06b17abbd23b0e68a38dc23b74977a
DigiDevon <digidevon@aol.com>
2023-10-06 05:14:46 -0600

    issue added to code

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+
 echo "Setting up Nix OS Virtual Machine from DigiDevon"
 echo "Making VM directory at /vm"
 sudo mkdir /vm
@@ -12,6 +13,7 @@ echo "Running Garbage Collection"
 echo "Installing flatpak repo is not already installed"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 echo "Creating autostart dir for win98 user"
+## ISSUES WITH CREATING AUTOSTART https://github.com/GeekCafe-XYZ/nixos-86box/issues/1
 sudo mkdir /home/win98/.config/autostart
 echo "Moving starup files to win98 user"
 sudo cp startup-win98.desktop /home/win98/.config/autostart
