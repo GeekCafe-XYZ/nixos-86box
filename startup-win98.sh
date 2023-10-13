7c59415c5fc65bf551d19b42d2d1ecd79f775cc2
Tinkerererer <61923195+ChiropracticBender@users.noreply.github.com>
2023-10-13 05:00:18 -0600

    Update startup-win98.sh
    --full-screen added to see if 86box launches in fullscreen mode when auto login starts.

--- startup-win98.sh
+++ startup-win98.sh
@@ -3,7 +3,7 @@ flatpak update
 echo "Installing 86Box"
 flatpak install flathub net._86box._86Box -y
 echo "Starting 86Box"
-flatpak run net._86box._86Box
+flatpak run net._86box._86Box --full-screen
 echo "Creating Home Directory Folders"
 mkdir /home/win98/Floppy\ Disks/
 mkdir /home/win98/CDs
